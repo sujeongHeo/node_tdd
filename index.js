d6d9f732fb38acd5d1c0f8177248fb5b108f11a7
sujeongHeo <gjtnwjd40@gmail.com>
2020-08-01 21:28:40 +0900

    에러 미들웨어 추가 !

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express= require('express');
 const logger3 = require('morgan');
 const app = express(); 
+
+function commonmw(req, res, next) {
+     console.log('commonmw');
+     next(new Error('erro ouccured'));
+}
+
+
+function errormw(err, req, res, next){
+    console.log('sdfsdf', err.message);
+    // 에러를 처리하거나 
+    next (err);
+}
+
+
+app.use(commonmw);
+app.use(errormw);
 // 익스프레스 인스턴스 ( 서버에 필요한 미들웨어)
 // 서버를 요청 대기 상태로 만들 수 있음 
 // 미들웨어 (함수들의 연속) 만들기 
@@ -13,11 +29,6 @@ function logger2(req, res, next){
     console.log('I am logger2');
     next();
 }
-app.use(logger);
-app.use(logger2);
-app.use(logger3('dev'));
-
-
 // 서드 파티 익스프레스 가져오기  !! morgan , 서버 쪽에 로그 남기는 미들웨어
 app.listen(3000, function(){
     console.log('Server is running');
