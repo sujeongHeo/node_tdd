bf7a0abb910b14872213221024d0e1e7b90eef33
sujeongHeo <gjtnwjd40@gmail.com>
2020-08-11 01:32:50 +0900

    성공하는 코드를 만들었지만 이해는 실패했다

--- index.spec.js
+++ index.spec.js
@@ -50,3 +50,27 @@ describe('GET /users/1', () => {
         });
     });   
 });
+
+describe('POST /users', () => {
+    describe('성공시', () => {
+        let name = 'daniel',
+            body;
+        before(done=> {
+            request(app)
+            .post('/users')
+            .send({name})
+            .expect(201)
+            .end((err, res) => {
+              body = res.body;
+              done();
+            });
+
+        });
+        it('생성된 유저 객체를 반환한다.', ()=> {
+            body.should.have.property('id');
+        });
+        it ('입력한 name 을 반환한다.', ()=> {
+            body.should.have.property('name', name)
+        })
+    })
+})
