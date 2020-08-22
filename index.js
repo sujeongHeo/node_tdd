5c8c400852ce942cc56404b12928dcf363bc2ee7
sujeongHeo <gjtnwjd40@gmail.com>
2020-08-22 17:03:59 +0900

    중복된 라우터 설정으로 인해서  index 에서는 지워줌

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ var bodyParser = require('body-parser');
 var user = require('./api/user');
 const port = 3000
 
+if (process.env.NODE_ENV !== 'test'){
+  app.use(morgan('dev'));
+}
 
-app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/users', user);
 
-app.listen(port, function () {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+
 
 module.exports = app;
